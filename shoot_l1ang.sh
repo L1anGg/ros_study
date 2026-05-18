98ca04cfeaef48d486e56c6fcca7dda36f66875d
L1anG <2563631867@qq.com>
2026-05-18 23:45:48 +0800

    debug定位文件添加定点导航pid

--- shoot_l1ang.sh
+++ shoot_l1ang.sh
@@ -2,7 +2,7 @@
 
 # 仿真环境启动脚本 (Ubuntu 18.04 + ROS Melodic)
 
-gnome-terminal --window -e 'bash -c "sleep 6; source ~/abot_ws/devel/setup.bash; roslaunch gazebo_pkg race.launch; exec bash"' \
+gnome-terminal --window -e 'bash -c "sleep 1; source ~/abot_ws/devel/setup.bash; roslaunch gazebo_pkg race.launch; exec bash"' \
 --tab -e 'bash -c "sleep 5; source ~/abot_ws/devel/setup.bash; roslaunch robot_slam navigation_shoot.launch; exec bash"' \
 --tab -e 'bash -c "sleep 6; source ~/abot_ws/devel/setup.bash; roslaunch find_object_2d find_object_2d_shoot.launch; exec bash"' \
 --tab -e 'bash -c "sleep 3; source ~/abot_ws/devel/setup.bash; rosrun xunfei_py_ros iat_node.py; exec bash"' \
